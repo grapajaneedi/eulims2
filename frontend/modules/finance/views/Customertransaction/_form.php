fac771c499cd012a3de42b453cdd94910825b875
Bergel <b.cutara@gmail.com>
2018-05-28 14:06:33 +0800

    Revert Files from pullmerge #22
    Issue: My updates are gone and missing

--- frontend/modules/finance/views/Customertransaction/_form.php
+++ frontend/modules/finance/views/Customertransaction/_form.php
@@ -1,8 +1,6 @@
 <?php
-
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
 /* @var $this yii\web\View */
 /* @var $model common\models\finance\Customertransaction */
 /* @var $form yii\widgets\ActiveForm */
@@ -13,13 +11,15 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin([
     ]); ?>
 
-    <?= $form->field($model, 'amount')->textInput(['maxlength' => true,'type'=>'number']) ?>
+    <?= $form->field($model, 'amount')->textInput(['maxlength' => true,'type'=>'number','onChange'=>'myFunction(this.value)']) ?>
 
     <?= $form->field($model, 'customerwallet_id')->hiddenInput(['value' => $customerwallet_id])->label(false); ?>
 
     <div class="form-group pull-right">
         
-        <?= Html::submitButton($model->isNewRecord ? 'Add' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary','data' => [
+        <?= Html::submitButton($model->isNewRecord ? 'Add' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success mybtn' : 'btn btn-primary mybtn',
+            'disabled'=>true,
+            'data' => [
             'confirm' => 'Are you sure you want to add credits to this account ?',
         ],]) ?>
         <?php if(Yii::$app->request->isAjax){ ?>
@@ -31,4 +31,13 @@ use yii\widgets\ActiveForm;
 
     <?php ActiveForm::end(); ?>
 
+
 </div>
+<script>
+function myFunction(val) {
+    if((val!=0)||(val!=""))
+        $('.mybtn').prop('disabled',false);
+    else
+        $('.mybtn').prop('disabled',true);
+}
+</script>
