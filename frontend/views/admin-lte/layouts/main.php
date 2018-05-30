74b9de8a4a03e6d2fed0682214c8db3a34b15bb1
Nolan Sunico <nolansunico@gmail.com>
2018-05-30 15:02:33 +0800

    update

--- frontend/views/admin-lte/layouts/main.php
+++ frontend/views/admin-lte/layouts/main.php
@@ -40,6 +40,50 @@ if (Yii::$app->controller->action->id === 'login') {
                 var url="/reports/preview?url="+PDFUrl;
                 window.open(url, '_blank');
             }
+                        function CurrencyFormat(number,decimalplaces){
+               if (typeof decimalplaces === 'undefined'){ 
+                   decimalplaces = 2; 
+               }
+               var decimalcharacter = ".";
+               var thousandseparater = ",";
+               number = parseFloat(number);
+               var sign = number < 0 ? "-" : "";
+               var formatted = new String(number.toFixed(decimalplaces));
+               if( decimalcharacter.length && decimalcharacter != "." ) { formatted = formatted.replace(/\./,decimalcharacter); }
+               var integer = "";
+               var fraction = "";
+               var strnumber = new String(formatted);
+               var dotpos = decimalcharacter.length ? strnumber.indexOf(decimalcharacter) : -1;
+               if( dotpos > -1 )
+               {
+                  if( dotpos ) { integer = strnumber.substr(0,dotpos); }
+                  fraction = strnumber.substr(dotpos+1);
+               }
+               else { integer = strnumber; }
+               if( integer ) { integer = String(Math.abs(integer)); }
+               while( fraction.length < decimalplaces ) { fraction += "0"; }
+               temparray = new Array();
+               while( integer.length > 3 )
+               {
+                  temparray.unshift(integer.substr(-3));
+                  integer = integer.substr(0,integer.length-3);
+               }
+               temparray.unshift(integer);
+               integer = temparray.join(thousandseparater);
+               return sign + integer + decimalcharacter + fraction;
+            }
+            function StringToFloat(str, decimalForm){
+                    //This function will convert string value into Float valid values
+                    if (typeof decimalForm === 'undefined'){ 
+                            decimalForm = 2; 
+                    }
+                    var v=str.replace(',','').replace(' ','');
+                    v=v.replace(',','').replace(' ','');
+                    v=parseFloat(v);
+                    //console.log(v);
+                    var v=v.toFixed(decimalForm);
+                    return v;
+            }
         </script>
     </head>
     <body class="hold-transition skin-blue sidebar-collapse">
