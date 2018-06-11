825472f98a8d31f24c9496d243977deedd0e764f
Nolan Sunico <nolansunico@gmail.com>
2018-06-11 16:37:18 +0800

    update on Request

--- frontend/modules/lab/views/request/_form.php
+++ frontend/modules/lab/views/request/_form.php
@@ -8,23 +8,39 @@ use common\models\lab\Lab;
 use kartik\datetime\DateTimePicker;
 use common\models\lab\Customer;
 use yii\web\JsExpression;
-use kartik\datecontrol\DateControl;
+use kartik\widgets\DatePicker;
 use common\models\lab\Paymenttype;
 use common\models\lab\Discount;
 use common\models\lab\Modeofrelease;
 use common\models\lab\Purpose;
+use kartik\widgets\SwitchInput;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\lab\Request */
 /* @var $form yii\widgets\ActiveForm */
-$JS=<<<SCRIPT
-    $("input[name=Request[payment_type_id]]").on('change', function() {
-        if ($(this).val() == 1) {
-           alert("OK");
-        }
-    }); 
+
+// script to parse the results into the format expected by Select2
+$dataExp = <<< SCRIPT
+  function (params, page) {
+    return {
+      q: params.term, // search term
+    };
+  }
 SCRIPT;
-//$this->registerJs($JS);
+
+$dataResults = <<< SCRIPT
+  function (data, page) {
+    return {
+      results: data.results
+    };
+  }
+SCRIPT;
+$disabled= $model->posted ? true:false;
+if($disabled){
+    $Color="#eee";
+}else{
+    $Color="white";
+}
 ?>
 
 <div class="request-form">
@@ -35,14 +51,13 @@ SCRIPT;
     <?= $form->field($model, 'status_id')->hiddenInput()->label(false) ?>
     <?= $form->field($model, 'modeofrelease_ids')->hiddenInput(['id'=>'modeofrelease_ids'])->label(false) ?>
     <?= $form->field($model, 'created_at')->hiddenInput()->label(false) ?>
-    <?= $form->field($model, 'report_due')->hiddenInput()->label(false) ?>
     <?= $form->field($model, 'total')->hiddenInput(['maxlength' => true])->label(false) ?>
 <div class="row">
     <div class="col-md-6">
     <?= $form->field($model, 'lab_id')->widget(Select2::classname(), [
         'data' => ArrayHelper::map(Lab::find()->all(),'lab_id','labname'),
         'language' => 'en',
-        'options' => ['placeholder' => 'Select Lab'],
+        'options' => ['placeholder' => 'Select Lab','disabled'=>false],
         'pluginOptions' => [
             'allowClear' => true
         ]
@@ -54,6 +69,7 @@ SCRIPT;
 	'model' => $model,
 	'attribute' => 'request_datetime',
         'readonly'=>true,
+        'disabled'=>$disabled,
 	'options' => ['placeholder' => 'Enter Date'],
         'value'=>function($model){
              return date("m/d/Y h:i:s P",$model->request_datetime);
@@ -74,10 +90,10 @@ SCRIPT;
     <?php
     $url = \yii\helpers\Url::to(['customerlist']);
     // Get the initial city description
-    $cust_name = empty($model->customer) ? '' : Customer::findOne($model->customer)->customer_name;
+    $cust_name = empty($model->customer) ? '' : Customer::findOne($model->customer_id)->customer_name;
     echo $form->field($model, 'customer_id')->widget(Select2::classname(), [
         'initValueText' => $cust_name, // set the initial display text
-        'options' => ['placeholder' => 'Search for a customer ...'],
+        'options' => ['placeholder' => 'Search for a customer ...','disabled'=>$disabled],
         'pluginOptions' => [
             'allowClear' => true,
             'minimumInputLength' => 3,
@@ -87,11 +103,9 @@ SCRIPT;
             'ajax' => [
                 'url' => $url,
                 'dataType' => 'json',
-                'data' => new JsExpression('function(params) { return {q:params.term,id:1}; }')
-            ],
-            'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
-            'templateResult' => new JsExpression('function(Customer) { return Customer.text; }'),
-            'templateSelection' => new JsExpression('function (Customer) { return Customer.text; }'),
+                'data' => new JsExpression($dataExp),
+                'results' => new JsExpression($dataResults)
+            ]
         ],
     ])->label('Customer');
     ?>
@@ -100,7 +114,8 @@ SCRIPT;
         <label class="control-label">Payment Type</label>
         <div class="col-md-12">
         <?php echo $form->field($model, 'payment_type_id')->radioList(
-            ArrayHelper::map(Paymenttype::find()->all(),'payment_type_id','type')
+            ArrayHelper::map(Paymenttype::find()->all(),'payment_type_id','type'),
+            ['itemOptions' => ['disabled' => $disabled]]
         )->label(false); ?>
         </div>
     </div>
@@ -108,15 +123,16 @@ SCRIPT;
 <div class="row">
     <div class="col-md-6">
     <?php  
-    echo '<label class="control-label">Mode of ReleaseIDs</label>';
+    echo '<label class="control-label">Mode of Release</label>';
     echo Select2::widget([
         'name' => 'modeofreleaseids',
         'id' => 'modeofreleaseids',
         'data' => ArrayHelper::map(Modeofrelease::find()->all(),'modeofrelease_id','mode'),
-        'value'=>$model->modeofrelease_ids,
+        'value'=>explode(',',$model->modeofrelease_ids),
         'options' => [
             'placeholder' => 'Select provinces ...',
-            'multiple' => true
+            'multiple' => true,
+            'disabled'=>$disabled
         ],
         'pluginEvents' => [
             "change" => "function() { 
@@ -131,7 +147,7 @@ SCRIPT;
     <?= $form->field($model, 'discount_id')->widget(Select2::classname(), [
         'data' => ArrayHelper::map(Discount::find()->all(),'discount_id','type'),
         'language' => 'en',
-        'options' => ['placeholder' => 'Select Discount'],
+        'options' => ['placeholder' => 'Select Discount','disabled'=>$disabled],
         'pluginOptions' => [
             'allowClear' => true
         ],
@@ -155,22 +171,58 @@ SCRIPT;
     <?= $form->field($model, 'purpose_id')->widget(Select2::classname(), [
         'data' => ArrayHelper::map(Purpose::find()->all(),'purpose_id','name'),
         'language' => 'en',
-        'options' => ['placeholder' => 'Select Purpose'],
+        'options' => ['placeholder' => 'Select Purpose','disabled'=>$disabled],
         'pluginOptions' => [
             'allowClear' => true
         ],
     ])->label('Purpose'); ?>
     </div>
     <div class="col-md-6">
-    <?= $form->field($model, 'discount')->textInput(['maxlength' => true,'readonly'=>true,'style'=>'background-color: white']) ?>
+    <?= $form->field($model, 'discount')->textInput(['maxlength' => true,'readonly'=>true,'style'=>'background-color: '.$Color]) ?>
     </div>
 </div>
 <div class="row">
     <div class="col-md-6">
-    <?= $form->field($model, 'conforme')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'is_referral')->widget(SwitchInput::classname(), [
+        'disabled' => $disabled,
+        'pluginOptions' => [
+            'size' => 'small',
+            'handleWidth'=>'53',
+            'onColor' => 'success',
+            'offColor' => 'danger',
+            'onText' => 'Yes',
+            'offText' => 'No',
+        ]
+    ])->label("Referral"); ?>
     </div>
     <div class="col-md-6">
-    <?= $form->field($model, 'receivedBy')->textInput(['maxlength' => true]) ?>
+    <label class="control-label">Report Due</label>
+    <?php echo DatePicker::widget([
+	'model' => $model,
+	'attribute' => 'report_due',
+        'readonly'=>true,
+        'disabled' => $disabled,
+	'options' => ['placeholder' => 'Report Due'],
+        'value'=>function($model){
+             return date("m/d/Y",$model->report_due);
+        },
+	'pluginOptions' => [
+            'autoclose' => true,
+            'removeButton' => false,
+            'format' => 'yyyy-mm-dd'
+	],
+        'pluginEvents'=>[
+            "change" => "function() { alert('change'); }",
+        ]
+    ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+    <?= $form->field($model, 'conforme')->textInput(['readonly' => $disabled]) ?>
+    </div>
+    <div class="col-md-6">
+    <?= $form->field($model, 'receivedBy')->textInput(['readonly' => true]) ?>
     </div>
 </div>
 <div class="row">
