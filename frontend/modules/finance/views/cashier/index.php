ac8b7c4e3551e6fcfb45c46db0e7b130372121e6
blakadm <blakadm@gmail.com>
2018-06-14 16:14:37 +0800

    Cashier Dashboard
    Cashier Dashboard

--- frontend/modules/finance/views/cashier/index.php
+++ frontend/modules/finance/views/cashier/index.php
@@ -1,9 +1,24 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>cashier/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+
+<div class="box box-primary box-solid" style="background:transparent">
+    <div class="box-header with-border">
+              <h2 class="box-title">Cashier Dashboard</h2>
+
+              <!-- /.box-tools -->
+    </div>
+
+    <div class="box-body">
+   
+    <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/receiptsquare.png" style="height:150px"/>Receipt</div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/ordersquare.png" style="height:150px"/>Order of Payment</div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/depositsquare.png" style="height:150px"/>Deposit</div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/reportsquare.png" style="height:150px"/>Reports</div>
+    </div>
+
+    </div>
+    
+</div>
