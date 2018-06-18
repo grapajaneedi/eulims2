d8137395626b5bb8efc0458f026e8ecca59c8e2a
blakadm <blakadm@gmail.com>
2018-06-18 16:43:52 +0800

    Billing Dashboard, Cashier and accounting dashboard update
    Billing Dashboard, Cashier and accounting dashboard update

--- frontend/modules/finance/views/accounting/index.php
+++ frontend/modules/finance/views/accounting/index.php
@@ -1,9 +1,29 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>accounting/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<style type="text/css">
+
+img:hover{
+    border:5px solid #3c8dbc; 
+    border-radius: 25px;
+}
+</style>
+
+<div class="box box-primary box-solid" style="background:transparent">
+    <div class="box-header with-border">
+              <h2 class="box-title">Accounting Dashboard</h2>
+
+              <!-- /.box-tools -->
+    </div>
+
+    <div class="box-body">
+   
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12" style="text-align:center"><a href="#"><img class="img-responsive center-block" src="/images/ordersquare.png" style="height:150px;"/></a></div>
+        <div class="col-md-6 col-sm-6 col-xs-12" style="text-align:center"><a href="#"><img class="img-responsive center-block" src="/images/orderlabsquare.png" style="height:150px"/></a></div>
+    </div>
+
+    </div>
+    
+</div>
--- frontend/modules/finance/views/cashier/index.php
+++ frontend/modules/finance/views/cashier/index.php
@@ -1,6 +1,16 @@
 <?php
 /* @var $this yii\web\View */
+//<div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="/finance/cashier/reports/"><img class="img-responsive center-block" src="/images/reportsquare.png" style="height:150px"/>Reports</a></div>
+       
 ?>
+
+<style type="text/css">
+
+img:hover{
+    border:5px solid #3c8dbc; 
+    border-radius: 25px;
+}
+</style>
 
 
 <div class="box box-primary box-solid" style="background:transparent">
@@ -13,12 +23,15 @@
     <div class="box-body">
    
     <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/receiptsquare.png" style="height:150px"/>Receipt</div>
-        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/ordersquare.png" style="height:150px"/>Order of Payment</div>
-        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/depositsquare.png" style="height:150px"/>Deposit</div>
-        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><img class="img-responsive center-block" src="/images/reportsquare.png" style="height:150px"/>Reports</div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="/finance/cashier/receipt/"><img class="img-responsive center-block" src="/images/receiptsquare.png" style="height:150px"/></a></div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="/finance/cashier/op/"><img class="img-responsive center-block" src="/images/ordersquare.png" style="height:150px"/></a></div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="/finance/cashier/deposit/"><img class="img-responsive center-block" src="/images/depositsquare.png" style="height:150px"/></a></div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="/finance/cashier/reports/"><img class="img-responsive center-block" src="/images/reportsquare.png" style="height:150px"/></a></div>
     </div>
 
     </div>
     
 </div>
+
+
+           
