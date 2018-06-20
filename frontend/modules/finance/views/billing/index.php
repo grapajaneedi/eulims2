99a135a2e877f2b27a25d32f4dfedd9ca63d4584
Nolan Sunico <nolansunico@gmail.com>
2018-06-20 15:33:10 +0800

    Update on Billing Dashboard

--- frontend/modules/finance/views/billing/index.php
+++ frontend/modules/finance/views/billing/index.php
@@ -1,5 +1,9 @@
 <?php
 /* @var $this yii\web\View */
+
+$this->title = 'Billing';
+$this->params['breadcrumbs'][] = ['label' => 'Finance', 'url' => ['/finance']];
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <style type="text/css">
@@ -24,7 +28,7 @@ img:hover{
         <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="#"><img class="img-responsive center-block" src="/images/invoicesquare.png" style="height:150px"/></a></div>
         <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="#"><img class="img-responsive center-block" src="/images/reportsquare.png" style="height:150px"/></a></div>
         <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="#"><img class="img-responsive center-block" src="/images/agingsquare.png" style="height:150px"/></a></div>
-        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="#"><img class="img-responsive center-block" src="/images/billingmanagersquare.png" style="height:150px"/></a></div>
+        <div class="col-md-3 col-sm-6 col-xs-12" style="text-align:center"><a href="/finance/billing/manager"><img class="img-responsive center-block" src="/images/billingmanagersquare.png" style="height:150px"/></a></div>
        
     </div>
    
