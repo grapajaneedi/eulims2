eece2041f07cf1c7e4b6cd3f790f84d59c9f4722
Eden Galleno <gallenoeden09@gmail.com>
2018-06-22 17:11:35 +0800

    Cashier
    # Updates on receipt
    # Added Check

---
+++ frontend/modules/finance/views/cashier/create_check.php
@@ -0,0 +1,131 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use kartik\select2\Select2;
+use kartik\widgets\DatePicker;
+use yii\helpers\Url;
+use common\components\Functions;
+/* @var $this yii\web\View */
+/* @var $model common\models\finance\Check */
+/* @var $form yii\widgets\ActiveForm */
+
+exit;
+?>
+
+<div class="receipt-form" style="margin:0important;padding:0px!important;padding-bottom: 10px!important;">
+
+    <?php $form = ActiveForm::begin(); ?>
+    <div class="alert alert-info" style="background: #d9edf7 !important;margin-top: 1px !important;">
+     <a href="#" class="close" data-dismiss="alert" >×</a>
+    <p class="note" style="color:#265e8d">Fields with <i class="fa fa-asterisk text-danger"></i> are required.</p>
+     
+    </div>
+   
+    <div style="padding:0px!important;">
+        <div class="row">
+            <div class="col-sm-6">
+             <?php echo $form->field($model, 'bank')->textInput() ?>
+            </div>   
+            <div class="col-sm-6">
+             <?php echo $form->field($model, 'checknumber')->textInput() ?>
+            </div> 
+        </div>
+        <div class="row">
+          
+            <div class="col-sm-6">
+           <?php
+             echo $form->field($model, 'checkdate')->widget(DatePicker::classname(), [
+             'options' => ['placeholder' => 'Select Date ...',
+             'autocomplete'=>'off'],
+             'type' => DatePicker::TYPE_COMPONENT_APPEND,
+                 'pluginOptions' => [
+                     'format' => 'yyyy-mm-dd',
+                     'todayHighlight' => true,
+                     'autoclose'=>true,   
+                 ]
+             ]);
+             ?>
+            </div>
+            <div class="col-sm-6">
+                <?php echo $form->field($model, 'amount')->textInput() ?>
+            </div>  
+        </div>
+      
+        <div class="form-group pull-right">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary',
+                'id'=>'createCheck']) ?>
+            <?php if(Yii::$app->request->isAjax){ ?>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+            <?php } ?>
+        </div>
+    </div>
+    <?php ActiveForm::end(); ?>
+ <?php
+    // This section will allow to popup a notification
+    $session = Yii::$app->session;
+    if ($session->isActive) {
+        $session->open();
+        if (isset($session['deletepopup'])) {
+            $func->CrudAlert("Deleted Successfully","WARNING");
+            unset($session['deletepopup']);
+            $session->close();
+        }
+        if (isset($session['updatepopup'])) {
+            $func->CrudAlert("Updated Successfully");
+            unset($session['updatepopup']);
+            $session->close();
+        }
+        if (isset($session['savepopup'])) {
+            $func->CrudAlert("Successfully Saved","SUCCESS",true);
+            unset($session['savepopup']);
+            $session->close();
+        }
+        if (isset($session['errorpopup'])) {
+            $func->CrudAlert("Transaction Error","ERROR",true);
+            unset($session['errorpopup']);
+            $session->close();
+        }
+        if (isset($session['checkpopup'])) {
+            $func->CrudAlert("Insufficient Wallet Balance","INFO",true,false,false);
+            unset($session['checkpopup']);
+            $session->close();
+        }
+    }
+?>
+</div>
+<style>
+    .modal-body{
+        padding-top: 0px!important;
+    }
+</style>
+<script type="text/javascript">
+    $('#receipt-or_number').on('change',function(e) {
+        jQuery.ajax( {
+            type: 'POST',
+            url: '/finance/cashier/nextor?id='+$(this).val(),
+            dataType: 'json',
+            success: function ( response ) {
+                if(response.success === true)
+                {
+                    //$("span.btn").appendClass("btn-success");
+                    $('span.btn').removeClass("btn-warning");
+                    $('span.btn').addClass("btn-success");
+                    $('#next_or').html(response.nxtOR);
+                    $('#receipt-or').val(response.nxtOR);
+                } else {
+                    //$("span.btn").appendClass("btn-warning");
+                    $('span.btn').addClass("btn-warning");
+                    $('#next_or').html(response.nxtOR);
+                    $('#receipt-or').val('');
+                }
+            },
+            error: function ( xhr, ajaxOptions, thrownError ) {
+                alert( thrownError );
+            }
+        });
+       // alert('zdfsdf');
+       
+    });
+</script>
