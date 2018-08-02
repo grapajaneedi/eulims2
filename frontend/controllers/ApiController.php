79375c59ccdde8225ae2e21a6361eeb987076bbd
Bergel <b.cutara@gmail.com>
2018-08-02 10:25:23 +0800

    api controller a and chem iso

--- frontend/controllers/ApiController.php
+++ frontend/controllers/ApiController.php
@@ -8,6 +8,9 @@ use common\models\system\Profile;
 use kartik\mpdf\Pdf;
 use yii\helpers\Json;
 use common\models\lab\Customer;
+use common\models\lab\Request;
+use common\models\lab\Sample;
+use common\models\lab\Analysis;
 
 class ApiController extends ActiveController
 {
@@ -65,4 +68,113 @@ class ApiController extends ActiveController
             echo $ctr;
 
     }
+
+    public function actionSync_request(){ 
+        $myvar = Json::decode($_POST['data']);
+        $ctr = 0;
+            foreach ($myvar as $var) {
+
+                $newRequest = new Request();
+                $newRequest->request_ref_num=$var['request_ref_num'];
+                $newRequest->request_datetime=$var['request_datetime'];
+                $newRequest->rstl_id=$var['rstl_id'];
+                $newRequest->lab_id=$var['lab_id'];
+                $newRequest->customer_id=$var['customer_id'];
+                $newRequest->payment_type_id=$var['payment_type_id'];
+                $newRequest->modeofrelease_ids=$var['modeofrelease_ids'];
+                $newRequest->discount=$var['discount'];
+                $newRequest->purpose_id=$var['purpose_id'];
+                $newRequest->conforme=$var['conforme'];
+                $newRequest->report_due=$var['report_due'];
+                $newRequest->total=$var['total'];
+                $newRequest->receivedBy =$var['receivedBy'];
+                $newRequest->oldColumn_requestId=$var['oldColumn_requestId'];
+                $newRequest->oldColumn_sublabId=$var['oldColumn_sublabId'];
+                $newRequest->oldColumn_orId=$var['oldColumn_orId'];
+                $newRequest->oldColumn_completed=$var['oldColumn_completed'];
+                $newRequest->oldColumn_cancelled=$var['oldColumn_cancelled'];
+                $newRequest->oldColumn_create_time=$var['oldColumn_create_time'];
+                $newRequest->request_old_id=$var['request_old_id'];
+                $newRequest->created_at=0;
+                $newRequest->discount_id=0;
+                if($newRequest->save()){
+                    $ctr++;
+                }
+            }
+            echo $ctr;
+
+    }
+
+
+    public function actionSync_sample(){ 
+        $myvar = Json::decode($_POST['data']);
+        $ctr = 0;
+            foreach ($myvar as $var) {
+
+                $newSample = new Sample();
+                $newSample->rstl_id=$var['rstl_id'];
+                $newSample->pstcsample_id=$var['pstcsample_id'];
+                $newSample->sample_type_id=$var['sample_type_id'];
+                $newSample->sample_code=$var['sample_code'];
+                $newSample->samplename=$var['samplename'];
+                $newSample->description=$var['description'];
+                $newSample->sampling_date=$var['sampling_date'];
+                $newSample->remarks=$var['remarks'];
+                $newSample->request_id=$var['request_id'];
+                $newSample->sample_month=$var['sample_month'];
+                $newSample->sample_year=$var['sample_year'];
+                $newSample->active=$var['active'];
+                $newSample->sample_old_id=$var['sample_old_id'];
+                $newSample->oldColumn_requestId=$var['oldColumn_requestId'];
+                $newSample->oldColumn_completed=$var['oldColumn_completed'];
+                $newSample->oldColumn_datedisposal=$var['oldColumn_datedisposal'];
+                $newSample->oldColumn_mannerofdisposal=$var['oldColumn_mannerofdisposal'];
+                $newSample->oldColumn_batch_num=$var['oldColumn_batch_num'];
+                $newSample->oldColumn_package_count=$var['oldColumn_package_count'];
+                $newSample->testcategory_id=0;
+                if($newSample->save()){
+                    $ctr++;
+                }
+            }
+            echo $ctr;
+
+    }
+
+
+    public function actionSync_analysis(){ 
+        $myvar = Json::decode($_POST['data']);
+        $ctr = 0;
+            foreach ($myvar as $var) {
+                $newanalysis = new Analysis();
+                $newanalysis->rstl_id=$var['rstl_id'];
+                $newanalysis->pstcanalysis_id=$var['pstcanalysis_id'];
+                $newanalysis->sample_id=$var['sample_id'];
+                $newanalysis->sample_code=$var['sample_code'];
+                $newanalysis->testname=$var['testname'];
+                $newanalysis->method=$var['method'];
+                $newanalysis->references=$var['references'];
+                $newanalysis->quantity=$var['quantity'];
+                $newanalysis->fee=$var['fee'];
+                $newanalysis->test_id=$var['test_id'];
+                $newanalysis->cancelled=$var['cancelled'];
+                $newanalysis->date_analysis=$var['date_analysis'];
+                $newanalysis->user_id=$var['user_id'];
+                $newanalysis->is_package=$var['is_package'];
+                $newanalysis->oldColumn_deleted=$var['oldColumn_deleted'];
+                $newanalysis->analysis_old_id=$var['analysis_old_id'];
+                $newanalysis->oldColumn_taggingId=$var['oldColumn_taggingId'];
+                $newanalysis->oldColumn_result=$var['oldColumn_result'];
+                $newanalysis->oldColumn_package_count=$var['oldColumn_package_count'];
+                $newanalysis->oldColumn_requestId=$var['oldColumn_requestId'];
+                $newanalysis->request_id=0;
+                $newanalysis->testcategory_id=0;
+                $newanalysis->sample_type_id=0;
+                if($newanalysis->save()){
+                    $ctr++;
+                }
+            }
+            echo $ctr;
+    }
+
+
 }
