8a4125990aa61dfbffba48324d214d6f78380a83
denden23 <gallenoeden09@gmail.com>
2018-09-14 16:58:55 +0800

    api
    # customer

--- common/modules/system/controllers/UtilitiesController.php
+++ common/modules/system/controllers/UtilitiesController.php
@@ -12,10 +12,19 @@ namespace common\modules\system\controllers;
 
 use Yii;
 use yii\web\Controller;
+use linslin\yii2\curl;
+use yii\helpers\Json;
 use common\models\finance\Op;
 use common\models\finance\Paymentitem;
 use common\models\finance\SubsidiaryCustomer;
 use common\models\finance\Receipt;
+use common\models\finance\Accountingcode;
+use common\models\finance\Accountingcodemapping;
+use common\models\finance\Billing;
+use common\models\finance\Soa;
+use common\models\finance\SoaBilling;
+use common\models\finance\SoaReceipt;
+use common\models\lab\Customer;
 /**
  * Description of BackupController
  *
@@ -34,6 +43,9 @@ class UtilitiesController extends Controller {
         $paymentitem=Paymentitem::find()->where(['between','orderofpayment_id',1,10])->all();
         $sc= SubsidiaryCustomer::find()->where(['between','orderofpayment_id',1,10])->all();
         $receipt= Receipt::find()->where(['between','orderofpayment_id',1,10])->all();
+      //  $accounting_code= Accountingcode::find()->where(['between','orderofpayment_id',1,10])->all();
+        $billing= Billing::find()->where(['between','billing_id',1,10])->all();
+        $soa= Soa::find()->where(['between','soa_id',1,10])->all();
         $sync=[
             'sync_log'=>[
                 'date_sync'=>'09/11/2018 10:01:12',
@@ -56,14 +68,100 @@ class UtilitiesController extends Controller {
             'paymentitem'=>$paymentitem,
             'subsidiary_customer'=>$sc,
             'receipt'=>$receipt,
+            'billing'=>$billing,
+            'soa'=>$soa,
             
         ];
+       
+//        Yii::$app->response->format= \yii\web\Response::FORMAT_JSON;
+//        return $sync;
+    
        // $content = json_encode($sync);
-        Yii::$app->response->format= \yii\web\Response::FORMAT_JSON;
-        return $sync;
-       //return $this->render('backup_finance');
+        $curl = new curl\Curl();
+        $url="http://api3.onelab.ph.local/finance/sync-finance";
+        
+        $response = $curl->setRequestBody(json_encode($sync))
+        /*->setHeaders([
+           'Content-Type' => 'application/json',
+           'Content-Length' => strlen(json_encode($sync))
+        ])*/
+        ->post($url);
+        return $response;
     }
     public function actionBackupInventory(){
         return "Inventory";
+    }
+    public function actionRestoreCustomer(){
+        $model = new Customer();
+        $rstl_id=$model->rstl_id=Yii::$app->user->identity->profile->rstl_id;
+        
+        $apiUrl="https://api3.onelab.ph/get/customer/view?rstl_id=11";
+        $curl = new curl\Curl();
+        $res = $curl->get($apiUrl);
+        
+//          for($i=0;$i<$arr_length;$i++){
+//            $request =$this->findRequest($str_request[$i]);
+//            $paymentitem = new Paymentitem();
+//            $paymentitem->rstl_id =Yii::$app->user->identity->profile ? Yii::$app->user->identity->profile->rstl_id : 11;
+//            $paymentitem->request_id = $str_request[$i];
+//            $paymentitem->orderofpayment_id = $opid;
+//            $paymentitem->details =$request->request_ref_num;
+//            $total=$request->total;
+//            $amount=$request->getBalance($str_request[$i],$total);
+//            $total_amount+=$amount;
+//            $paymentitem->amount = $amount;
+//            $paymentitem->request_type_id =$request->request_type_id;
+//            $paymentitem->status=1;//Unpaid
+//            $paymentitem->save(false); 
+//
+//        }
+//        $post=[];
+//            $posted=current($_POST['Paymentitem']);
+//            $post['Paymentitem']=$posted;
+//            if($paymentitem->load($post))
+//            {
+//                $paymentitem->save();
+//            }
+       // $customerlist= ArrayHelper::map( $decode,'lab_id','labname');
+       
+        $decode=Json::decode($res);
+        $exist=0;
+        $not_exist=0;
+        foreach ($decode as $customer){
+            $cust=Customer::find()->where(['customer_code'=>$customer['customer_code']])->all();
+            if($cust){
+               $exist+=1; 
+            }
+            else{
+                $model_customer = new Customer();
+                $model_customer->rstl_id=$rstl_id;
+                $model_customer->customer_code=$customer['customer_code'];
+                $model_customer->customer_name=$customer['customer_name'];
+                $model_customer->classification_id=$customer['classification_id'];
+                $model_customer->latitude=$customer['latitude'];
+                $model_customer->longitude=$customer['longitude'];
+                $model_customer->head=$customer['head'];
+                $model_customer->barangay_id=$customer['barangay_id'];
+                $model_customer->address=$customer['address'];
+                $model_customer->tel=$customer['tel'];
+                $model_customer->fax=$customer['fax'];
+                $model_customer->email=$customer['email'];
+                $model_customer->customer_type_id=$customer['customer_type_id'];
+                $model_customer->business_nature_id=$customer['business_nature_id'];
+                $model_customer->industrytype_id=$customer['industrytype_id'];
+                $model_customer->created_at=$customer['created_at'];
+                $model_customer->customer_old_id=$customer['customer_old_id'];
+                $model_customer->Oldcolumn_municipalitycity_id=$customer['Oldcolumn_municipalitycity_id'];
+                $model_customer->Oldcolumn_district=$customer['Oldcolumn_district'];
+                $model_customer->save();
+                $not_exist+=1;
+            }
+           
+        }
+         echo $exist;
+            echo "<br>";
+            echo $not_exist;  
+            
+     // return $res;
     }
 }
--- frontend/modules/api/controllers/CustomerController.php
+++ frontend/modules/api/controllers/CustomerController.php
@@ -9,6 +9,7 @@ use common\models\lab\Request;
 use common\models\lab\Sample;
 use common\models\lab\Analysis;
 use common\models\lab\Customer;
+use yii\helpers\Json;
 
 /**
  * Default controller for the `Lab` module
@@ -21,69 +22,57 @@ class CustomerController extends Controller
      */
     public function actionIndex()
     {
-        $requestquery = Request::find()->Where(['rstl_id'=>11])->andWhere(['between', 'request_id', 1, 1000])->all();  
-        
-                // $samplesquery = Sample::find()
-                // ->andWhere(['between', 'request_id', 1, 10])->all();
-        
-                // $analysisquery = Analysis::find()
-                // ->andWhere(['between', 'request_id', 1, 10])->all();
-        
-                $customerquery = Customer::find()
-                ->leftJoin('tbl_request', 'tbl_request.customer_id=tbl_customer.customer_id')
-                ->Where(['tbl_customer.rstl_id'=>11])
-                ->andWhere(['between', 'tbl_request.request_id', 1, 10000])->all();
-        
-                $syncarray = [
-                    'sync_log'=>[
-                        'date_sync'=>'09/11/2018 10:01:12',
-                        'rstl_id'=>11,
-                        'local_user_id'=>1,
-                        'sync_details'=>[
-                           [
-                             'table_name'=>'tbl_request',
-                             'start_id'=>'1',
-                             'last_id'=>10
-                           ],
-                           [
-                             'table_name'=>'tbl_sample',
-                             'start_id'=>'1',
-                             'last_id'=>10
-                           ]
-                        ]
-                    ],
-                    'tbl_customer'=> $customerquery
-                ];
-        
-                \Yii::$app->response->format= \yii\web\Response::FORMAT_JSON;
-             
-        
-               $content = json_encode($syncarray);
-               $LabURI="http://api3.onelab.ph.local/lab/getlabmodule";
-                $curl = new curl\Curl();
-        
-        
-                return $syncarray;
-                exit;
-         
-                $response = $curl->setOption(
-                    CURLOPT_POSTFIELDS, 
-                    http_build_query(array(
-                    'myPostField' => 'value'
-                     )
-                 ))
-               ->post('http://api3.onelab.ph.local/lab/getlabmodule');
-                      
-        
-        // return $response;
-        // exit;
-        //        return $response;
-        
-             //    $response = $curl->setRequestBody(json_encode($syncarray))
-                  //  ->setOption(CURLOPT_ENCODING, 'gzip')
-                //    ->post('http://api3.onelab.ph.local/lab/getlabmodule');
-                //return $response;
+        return $this->render('backup_restore');
+
     }
+
+    public function actionRestore(){
+         $model = new Customer();
+        $rstl_id=$model->rstl_id=Yii::$app->user->identity->profile->rstl_id;
+        
+        $apiUrl="https://api3.onelab.ph/get/customer/view?rstl_id=11&page=1&per-page=1000";
+        $curl = new curl\Curl();
+        $res = $curl->get($apiUrl);
+
+         $decode=Json::decode($res);
+        $exist=0;
+        $not_exist=0;
+        foreach ($decode as $customer){
+            $cust=Customer::find()->where(['customer_code'=>$customer['customer_code']])->all();
+            if($cust){
+               $exist++; 
+            }
+            else{
+                $model_customer = new Customer();
+                $model_customer->rstl_id=$rstl_id;
+                $model_customer->customer_code=$customer['customer_code'];
+                $model_customer->customer_name=$customer['customer_name'];
+                $model_customer->classification_id=$customer['classification_id'];
+                $model_customer->latitude=$customer['latitude'];
+                $model_customer->longitude=$customer['longitude'];
+                $model_customer->head=$customer['head'];
+                $model_customer->barangay_id=$customer['barangay_id'];
+                $model_customer->address=$customer['address'];
+                $model_customer->tel=$customer['tel'];
+                $model_customer->fax=$customer['fax'];
+                $model_customer->email=$customer['email'];
+                $model_customer->customer_type_id=$customer['customer_type_id'];
+                $model_customer->business_nature_id=$customer['business_nature_id'];
+                $model_customer->industrytype_id=$customer['industrytype_id'];
+                $model_customer->created_at=$customer['created_at'];
+                $model_customer->customer_old_id=$customer['customer_old_id'];
+                $model_customer->Oldcolumn_municipalitycity_id=$customer['Oldcolumn_municipalitycity_id'];
+                $model_customer->Oldcolumn_district=$customer['Oldcolumn_district'];
+                $model_customer->save();
+                $not_exist++;
+            }
+           
+        }
+
+        Yii::$app->session->setFlash('success', $not_exist.' Records Successfully Restored'); 
+        return $this->redirect(['index']);
+    }
+
 
     public function actionCustomer()
     {
