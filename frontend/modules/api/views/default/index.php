300543a457a7c5c3c6c21cd62d9fca41f60763b0
blakadm <blakadm@gmail.com>
2018-09-17 17:13:08 +0800

    Api Dashboard icons
    Api Dashboard icons

--- frontend/modules/api/views/default/index.php
+++ frontend/modules/api/views/default/index.php
@@ -1,12 +1,70 @@
+
+<style type="text/css">
+
+.imgHover:hover{
+    border-radius: 15px;
+    box-shadow: 0 0 0 4pt #3c8dbc;
+    transition: box-shadow 0.5s ease-in-out;
+}
+</style>
+
+
 <div class="Lab-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+   <div class="body-content">
+        <div class="box box-primary color-palette-box" style="padding:0px">
+        <div class="box-header" style="background-color: #fffff;color:#000" data-widget="collapse">
+          <h3 class="box-title" data-widget="collapse"><i class="fa fa-tag"></i>API Sync</h3>
+          <div class="box-tools pull-right" >
+                <button type="button" style="color:#000" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>
+        </div>      
+        <div class="box-body">
+          <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12">
+              
+               <div  style="padding-top: 1px;padding-bottom: 1px;display:block;text-align: center">
+                  <a href="#" title="Customer API"><img class="imgHover" src="/images/apicustomersquare.png" style="height:120px;width: 120px" ></a>
+              </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+            
+             <div  style="padding-top: 1px;padding-bottom: 1px;display:block;text-align: center">
+                 <a href="#" title="Finance API"><img class="imgHover" src="/images/apifinancesquare.png" style="height:120px;width: 120px"></a>
+              </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+              
+
+               <div  style="padding-top: 1px;padding-bottom: 1px;display:block;text-align: center">
+                   <a href="#" title="Laboratory API"><img class="imgHover" src="/images/apilaboratorysquare.png" style="height:120px;width: 120px"></a>
+              </div>
+            </div>
+            
+            <div class="col-md-3 col-sm-6 col-xs-12">
+           
+               <div  style="padding-top: 1px;padding-bottom: 1px;display:block;text-align: center">
+                 <a href="#" title="Services API"><img class="imgHover" src="/images/apiservicessquare.png" style="height:120px;width: 120px"></a>
+              </div>
+            </div>
+            
+            <!-- /.col -->
+            
+            <!-- /.col -->  
+          </div>
+        </div>
+        <!-- /.box-body -->
+        </div>
+        
+       
+        
+      
+        
+        
+        
+        
+        
+    </div>
 </div>
