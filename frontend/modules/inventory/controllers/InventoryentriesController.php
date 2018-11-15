15104f4a41bb3e20d1ea74dc020716072270b020
Bergel <b.cutara@gmail.com>
2018-11-15 08:33:48 +0800

    inventory update

--- frontend/modules/inventory/controllers/InventoryentriesController.php
+++ frontend/modules/inventory/controllers/InventoryentriesController.php
@@ -172,4 +172,11 @@ class InventoryentriesController extends Controller
         return $out;
     }
 
+    public function actionWithdraw(){
+
+          return $this->render('withdraw', [
+                // 'model' => $model,
+            ]);
+    }
+
 }
