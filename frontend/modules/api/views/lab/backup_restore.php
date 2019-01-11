cc3de2d7fa9fd73d2b4dc3b829de018ec1c1950d
grapajaneedi <grapajaneedi@gmail.com>
2019-01-11 14:59:41 +0800

    migration updates

--- frontend/modules/api/views/lab/backup_restore.php
+++ frontend/modules/api/views/lab/backup_restore.php
@@ -7,6 +7,7 @@ use yii\bootstrap\Progress;
 use kartik\grid\GridView;
 use common\models\lab\Sampletype;
 use common\models\lab\Services;
+use common\models\lab\Request;
 use common\models\lab\Lab;
 use common\models\lab\Testname;
 use yii\helpers\ArrayHelper;
@@ -26,62 +27,44 @@ use yii\helpers\Json;
 
 $func=new Functions();
 
-//   $apiUrl="https://eulimsapi.onelab.ph/api/web/v1/requests/restore?rstl_id=11&reqds=2015-01-01&reqde=2015-02-01&pp=5&page=1";
-//            
-//        $curl = curl_init();
-//				
-//		curl_setopt($curl, CURLOPT_URL, $apiUrl);
-//		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); //additional code
-//		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE); //additional code
-//		curl_setopt($curl, CURLOPT_FTP_SSL, CURLFTPSSL_TRY); //additional code
-//		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//
-//		$response = curl_exec($curl);
-//			
-//		$data = json_decode($response, true);
-//                
-//                print_r($data);
-//                exit;
 
-// $year = 2018;
-// $month_value = 01;
-// $apiUrl="https://eulimsapi.onelab.ph/api/web/v1/requests/restore?rstl_id=".$GLOBALS['rstl_id']."&reqds=".$year."-".$month_value."-01&reqde=".$year."-".$month_value."-31&pp=5&page=1";
-
-//         $curl = new curl\Curl();
-
-//         $curl->setOption(CURLOPT_SSL_VERIFYPEER, false);
-   
-//         $responselab = $curl->get($apiUrl);
-        
-//         $lab = Json::decode($responselab);
-//         var_dump($lab);
-//         exit;
-
-//echo $func->GetAccessToken(11);
-
-// $curl = new curl\Curl();
-// $response = $curl->get($apiUrl);
-
-//$decode=Json::decode($response);
-// echo '<pre>';
-// print_r($response);
-// echo '</pre>';
-// echo $response;
-
-// $sampletypelist= ArrayHelper::map(Sampletype::find()->all(),'sampletype_id','type');
-// $lablist= ArrayHelper::map(Lab::find()->all(),'lab_id','labname');
 
 $month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-$year = ['2013', '2014', '2015', '2016', '2017', '2018'];
+$year = ['2013', '2014', '2015', '2016', '2017', '2018', '2019'];
 
 //$lablist= ArrayHelper::map( $decode,'lab_id','labname');
 
 $this->title = 'Backup and Restore';
 $this->params['breadcrumbs'][] = ['label' => 'API', 'url' => ['/api']];
 $this->params['breadcrumbs'][] = $this->title;
+
+    
+        // $apiUrl="https://eulimsapi.onelab.ph/api/web/v1/requests/restoresyncnew?rstl_id=11&id=10148";
+        // $curl = curl_init();                   
+        // curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); //additional code
+        // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE); //additional code
+        // curl_setopt($curl, CURLOPT_FTP_SSL, CURLFTPSSL_TRY); //additional code
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        // $response = curl_exec($curl);
+                        
+        // $data = json_decode($response, true);
+
+        // echo "<pre>";
+        // var_dump($data);
+        // echo "</pre>";
+
 ?>
 
-<div class="services-index">
+<!-- <div class="alert alert-info" style="background: #d4f7e8 !important;margin-top: 1px !important;">
+     <a href="#" class="close" data-dismiss="alert" >×</a>
+    <p class="note" style="color:#265e8d"><b>EULIMS Migration</b></p>
+    <br>
+    <p class="note" style="color:#265e8d"><b>BY YEAR:</b> Choose a year</p>
+    <p class="note" style="color:#265e8d"><b>BY MONTH:</b> Choose a month and year</p>
+    <p class="note" style="color:#265e8d"><b>SYNC:</b> You can only use this if you have already migrated all the data by year</p>
+    </div>
+<div class="services-index"> -->
    
 <fieldset>
     <legend>Legend/Status</legend>
@@ -112,7 +95,9 @@ $this->params['breadcrumbs'][] = $this->title;
                         'theme' => Select2::THEME_KRAJEE,
                         'options' => ['id'=>'year'],
                         'pluginOptions' => ['allowClear' => true,'placeholder' => 'Select Year'],
-                ])->label("Year")."</div><div class='col-md-4' style='margin-top:4px'><br><span class='btn btn-success' id='offer' onclick='restorebyyear()'>RESTORE BY MONTH</span><div class='col-md-4' style='margin-top:4px'><span class='btn btn-success' id='offer' onclick='restore()'>RESTORE</span></div><div class='col-md-4' style='margin-top:4px'><span class='btn btn-success' id='offer' onclick='restore()'>SYNC</span></div>";
+                ])->label("Year")
+                ."</div>"."<div class='col-md-4' style='margin-top:4px'><br><span class='btn btn-success' id='offer' onclick='restore()'>BY YEAR</span>&nbsp;&nbsp;&nbsp;<span class='btn btn-success' id='offer' onclick='restorebyyear()'>BY MONTH</span>&nbsp;&nbsp;&nbsp;<span class='btn btn-success' id='sync' onclick='restore_sync  ()'>SYNC</span>";
+                //."</div><div class='col-md-4' style='margin-top:4px'><br><span class='btn btn-success' id='offer' onclick='restorebyyear()'>RESTORE BY MONTH</span><div class='col-md-4' style='margin-top:4px'><span class='btn btn-success' id='offer' onclick='restore()'>RESTORE</span></div><div class='col-md-4' style='margin-top:4px'><span class='btn btn-success' id='offer' onclick='restore()'>SYNC</span></div>";
             ?>
         </div>
         <?php ActiveForm::end(); ?>
@@ -120,8 +105,25 @@ $this->params['breadcrumbs'][] = $this->title;
     
   
     <div class = "row" style="padding-left:15px;padding-right:15px" id="methodreference">
+    <?php
+    //local and api number of request
 
-    <?= GridView::widget([
+    $request = Request::find()->count();
+
+    $GLOBALS['rstl_id']=Yii::$app->user->identity->profile->rstl_id;
+    $apiUrl="https://eulimsapi.onelab.ph/api/web/v1/requests/countrequest?rstl_id=11";        
+    $curl = curl_init();			
+    curl_setopt($curl, CURLOPT_URL, $apiUrl);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE); 
+    curl_setopt($curl, CURLOPT_FTP_SSL, CURLFTPSSL_TRY); 
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);			
+    $data = json_decode($response, true);
+    ?>
+    <?= 
+   
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'id'=>'testname-grid',
         'pjax' => true,
@@ -130,6 +132,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'panel' => [
                 'type' => GridView::TYPE_PRIMARY,
                 'heading' => '<span class="glyphicon glyphicon-book"></span>  ' . Html::encode($this->title),
+                'before'=> "<b>No. of Requests: </b>".$request."/ ".$data."",
                'after'=>false,
             ],
         'columns' => [
@@ -202,6 +205,28 @@ $this->params['breadcrumbs'][] = $this->title;
         
         $.ajax({
             url: "/api/lab/resyear",
+            method: "POST",
+            dataType: 'json',
+            data: {month:m, year:y},
+            beforeSend: function(xhr) {
+                $('.image-loader').addClass("img-loader");
+               }
+            })
+            .done(function( data ) {
+                alert(data.message);
+                $("#testname-grid").yiiGridView("applyFilter"); 
+
+                $('.image-loader').removeClass("img-loader");
+            });
+        }
+
+        function restore_sync(){
+
+        var m = $('#month option:selected').text();
+        var y = $('#year option:selected').text();
+        
+        $.ajax({
+            url: "/api/lab/ressync",
             method: "POST",
             dataType: 'json',
             data: {month:m, year:y},
