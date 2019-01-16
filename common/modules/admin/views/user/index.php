daf4491baad387402568f6d4d0913f27d6065ca0
grapajaneedi <grapajaneedi@gmail.com>
2019-01-16 14:35:04 +0800

    update

--- common/modules/admin/views/user/index.php
+++ common/modules/admin/views/user/index.php
@@ -16,7 +16,7 @@ $Header="Port Management System<br>";
 $Header.="List of Users";
 $dataProvider->pagination->pageSize=8;
 
-echo "test";
+
 ?>
 <div class="user-index">
     <?= $this->renderFile(__DIR__.'/../menu.php',['button'=>'user']); ?>
