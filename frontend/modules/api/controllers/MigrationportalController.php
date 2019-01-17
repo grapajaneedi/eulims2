dd1c0e6c7c599d9c1db87621b28b826b700dd3d3
Bergel <b.cutara@gmail.com>
2019-01-17 08:53:56 +0800

    migration portal and equipment inventory

--- frontend/modules/api/controllers/MigrationportalController.php
+++ frontend/modules/api/controllers/MigrationportalController.php
@@ -8,12 +8,17 @@ use common\models\api\MigrationportalSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use common\models\lab\Customer;
+use common\models\lab\CustomerMigration;
+use common\models\api\CustomerMigrationportal;
 
 /**
  * MigrationportalController implements the CRUD actions for Migrationportal model.
  */
 class MigrationportalController extends Controller
 {
+
+    private $batchlimit=100;
     /**
      * {@inheritdoc}
      */
@@ -38,9 +43,13 @@ class MigrationportalController extends Controller
         $searchModel = new MigrationportalSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
+        $fetchlings = Migrationportal::find()->all();
+        
+
         return $this->render('index', [
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
+            'fetchlings'=>$fetchlings,
         ]);
     }
 
@@ -123,5 +132,101 @@ class MigrationportalController extends Controller
         }
 
         throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+    public function actionFetch_customer($start){
+        // initiations
+        $connection= Yii::$app->labdb;
+
+        //fetch the the record by 1000
+        $fetch = CustomerMigration::find()->orderBy(["customer_id"=>SORT_ASC])->limit($this->batchlimit)->offset($start)->all();
+
+        if($fetch){
+            $transaction = $connection->beginTransaction();
+            try {
+                $ctr = 0;
+                foreach ($fetch as $fi) {
+                //use transaction here
+                    $newcustomer = new CustomerMigrationportal();
+                    $newcustomer->attributes = $fi->attributes;
+                    if($newcustomer->save()){
+                        $ctr++;
+                    }else{
+                        goto sadbear;
+                        break;
+                    }
+                }
+                    //if the compiler reach this area it just mean that all the record has been save and that the system will now update the tbl_migrationportal
+                    $transaction->commit();
+
+                    $fetchling = Migrationportal::find()->where(['table_name'=>'customer'])->one();
+                    $fetchling->date_migrated= date("Y-m-d");
+                    $fetchling->record_id=$start+$ctr;
+                    $fetchling->save(false);
+                    Yii::$app->session->setFlash('success', "Fetched ".$ctr." customer records");
+                
+            } catch (Exception $e) {
+                sadbear:
+                $transaction->rollback();
+            }
+        }
+        else{
+            Yii::$app->session->setFlash('error', "No new customer record/s found");
+        }
+        
+
+        return $this->redirect(['index']);
+    }
+
+    public function actionScript_customer($start){
+        // initiations
+        $connection= Yii::$app->labdb;
+
+        //fetch the the record by 1000
+        $fetch = CustomerMigrationPortal::find()->orderBy(["customer_id"=>SORT_ASC])->limit($this->batchlimit)->offset($start)->all();
+        if($fetch){
+            $transaction = $connection->beginTransaction();
+            try {
+                $ctr = 0;
+                $connection->createCommand('set foreign_key_checks=0')->execute();
+                foreach ($fetch as $fi) {
+                
+                    $fi->customer_code =  $fi->rstl_id."-".$fi->customer_old_id;
+                    if($fi->save(false)){
+                        $newrecord = new Customer();
+                        $newrecord->attributes = $fi->attributes;
+                        if($newrecord->save(false))
+                            $ctr++;
+                        else{
+                             goto sadbear;
+                             break;
+                        }
+                        
+                        
+                    }else{
+                        goto sadbear;
+                        break;
+                    }
+                }
+                $connection->createCommand('set foreign_key_checks=1')->execute();
+
+                //if the compiler reach this area it just mean that all the record has been save and that the system will now update the tbl_migrationportal
+                $transaction->commit();
+
+                $fetchling = Migrationportal::find()->where(['table_name'=>'customer'])->one();
+                $fetchling->date_migrated= date("Y-m-d");
+                $fetchling->record_idscript=$start+$ctr;
+                $fetchling->save(false);
+                Yii::$app->session->setFlash('success', "Processed ".$ctr." customer records");
+                
+            } catch (Exception $e) {
+                sadbear:
+                $transaction->rollback();
+            }
+        }
+        else{
+            Yii::$app->session->setFlash('error', "No new customer record/s to run the script");
+        }
+         return $this->redirect(['index']);
     }
 }
--- frontend/modules/api/views/migrationportal/index.php
+++ frontend/modules/api/views/migrationportal/index.php
@@ -3,6 +3,10 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\bootstrap\progress;
+use common\models\lab\CustomerMigration;
+use common\models\api\CustomerMigrationportal;
+
+
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\api\MigrationportalSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -11,188 +15,78 @@ $this->title = 'Migrationportals';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="migrationportal-index">
+    
+    <?php if (Yii::$app->session->hasFlash('success')): ?>
+        <div class="alert alert-success alert-dismissable">
+             <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+             <h4><i class="icon fa fa-check"></i>Saved!</h4>
+             <?= Yii::$app->session->getFlash('success') ?>
+        </div>
+    <?php endif; ?>
+    
+    <?php if (Yii::$app->session->hasFlash('error')): ?>
+        <div class="alert alert-danger alert-dismissable">
+             <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+             <h4><i class="icon fa fa-warning"></i>Note!</h4>
+             <?= Yii::$app->session->getFlash('error') ?>
+        </div>
+    <?php endif; ?>
 
-    <h1>Yii 1.0 to 2.0 Migration</h1>
+    <h1>Yii 1.0 to 2.0 Migration [Ulimsportal Only]</h1>
+    <?php
+    foreach ($fetchlings as $fetch) {
+        echo "<div>";
+        echo "<h3>TBL_".$fetch->table_name."</h3>";
 
-    <h3>customer</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>request</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>sample</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>analysis</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>check</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>receipt</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>deposit</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>payment Item</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>orderofpayment</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
-    ?>
-    <h3>collection</h3>
-    <div>
-        <?php 
-        echo Html::a('Fetch',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-         <?php 
-        echo Html::a('Process',array('syncrequest','start'=>0),['class'=>'btn btn-primary btn-small']);
-        ?>
-    </div>
-     <br>
-    <?php    // stacked bars
-        echo Progress::widget([
-            'bars' => [
-                ['percent' => 70,'label' => 'Processed', 'options' => ['class' => 'progress-success active']],
-                ['percent' => 30, 'label' => 'Pending', 'options' => ['class' => 'progress-warning']],
-            ]
-        ]);
+        if($fetch->table_name=="customer"){
+
+            $count_m = CustomerMigration::find()->count();
+            $count_mp = CustomerMigrationportal::find()->count();
+
+            echo Html::a('Fetch',array('fetch_customer','start'=>$fetch->record_id),['class'=>'btn btn-primary btn-small']);
+            echo "<br>";
+            echo "$count_mp  /$count_m records fetch from migration table";
+            echo "<br>";
+
+            //know the percentage
+            $perc_fetch=0;
+
+            if($count_m!=0){
+                $perc_fetch = ($fetch->record_id/$count_m)*100;
+                
+            }
+            $perc_fetch = number_format((float)$perc_fetch, 2, '.', '');
+            echo Progress::widget([
+                'percent' => $perc_fetch,
+                'label' => $perc_fetch.'% Fetch',
+                'options' => ['class' => 'progress-success active progress-striped']
+            ]);
+
+            //**********************
+            
+            echo Html::a('Run Script',array('script_customer','start'=>$fetch->record_idscript),['class'=>'btn btn-primary btn-small']);
+            echo "<br>";
+            echo "$fetch->record_idscript /$count_mp records process";
+            echo "<br>";
+            //know the percentage
+            $perc_fetch_mp=0;
+            if($count_mp!=0){
+                $perc_fetch_mp = ($fetch->record_idscript/$count_mp)*100;
+            }
+                 $perc_fetch_mp = number_format((float)$perc_fetch_mp, 2, '.', '');
+             echo Progress::widget([
+                'percent' => $perc_fetch_mp,
+                'label' => $perc_fetch_mp.'% Processed',
+                'options' => ['class' => 'progress-success  active progress-striped']
+            ]);
+
+
+
+        }
+        echo "</div>";
+    }
+
+
     ?>
 </div>
 
